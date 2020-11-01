7b3bbc69d7caf4abee335994fd559142032bf338
Ossamoon <osamu950623@gmail.com>
2020-11-01 22:25:33 +0900

    Displaying the recordings

---
+++ RecorderApp/RecorderApp/Helper.swift
@@ -0,0 +1,17 @@
+//
+//  Helper.swift
+//  RecorderApp
+//
+//  Created by 齋藤修 on 2020/11/01.
+//
+
+import Foundation
+
+func getCreationDate(for file: URL) -> Date {
+    if let attributes = try? FileManager.default.attributesOfItem(atPath: file.path) as [FileAttributeKey: Any],
+        let creationDate = attributes[FileAttributeKey.creationDate] as? Date {
+        return creationDate
+    } else {
+        return Date()
+    }
+}
